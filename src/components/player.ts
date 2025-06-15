30f44fe0f6739748971beeb4cfb34dad1fcfa9a0
JimJ92120 <jimj.development@gmail.com>
2025-06-16 00:16:55 +0700

    refactor: update player animation duration

--- src/components/player.ts
+++ src/components/player.ts
@@ -2,4 +2,4 @@ import Player, { Direction } from "../engine/components/Player";
 
 import PlayerSpriteUrl from "../assets/player.png";
 
-export default new Player([0, 0], Direction.Down, PlayerSpriteUrl, 200);
+export default new Player([0, 0], Direction.Down, PlayerSpriteUrl, 100);
